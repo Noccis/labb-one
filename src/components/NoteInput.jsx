051243f4442d58d469107aa0882bcd5d45d9bb29
Noccis <toniwincent83@gmail.com>
2024-11-02 20:53:05 +0100

    Created basic input form

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NoteInput = () => {
+  const [note, setNote] = useState({title: '', description: ''});
+
+  const handleChange = ((e)=>setNote({...note, [e.target.name]: e.target.value}));
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(note)
+  }
+
   return (
     <div>
-      
+      <form  onSubmit={handleSubmit}>
+        <input
+        type='text'
+        name='title'
+        value={note.title}
+        onChange={handleChange} />
+
+        <input
+        type='text'
+        name='description'
+        value={note.description}
+        onChange={handleChange} />
+
+        <input 
+        type='submit'/>        
+      </form>
     </div>
   )
 }
